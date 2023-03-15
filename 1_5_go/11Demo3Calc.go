a6fa5aff9cd0d4d2b10a3ddc10a4e6c54b2ea8d1
AmandaYi <891177434@qq.com>
2023-03-15 17:32:42 +0800

    calc多态

--- 1_5_go/11Demo3Calc.go
+++ 1_5_go/11Demo3Calc.go
@@ -1,26 +1,29 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var f CalcFactory
+	fmt.Println(f.Create("+", 1, 2))
 }
 
 //多态主要解决的是需求改变后，需要修改对象创建代码的问题
 //使用工厂类，内部调用多态函数，即可解决此问题
 
-type CaleCommon struct {
+type BaseCalc struct {
 	num1 int
 	num2 int
 }
 
-type ADDCalc struct {
-	CaleCommon
+type AddCalc struct {
+	BaseCalc
 }
 
 type ReduceCalc struct {
-	CaleCommon
+	BaseCalc
 }
 
-func (ac *ADDCalc) GetResult() int {
+func (ac *AddCalc) GetResult() int {
 	return ac.num1 + ac.num2
 }
 
@@ -28,20 +31,28 @@ func (rc *ReduceCalc) GetResult() int {
 	return rc.num1 - rc.num2
 }
 
-type CCalcFactory struct {
+type CalcER interface {
+	GetResult() int
 }
 
-func (cc *CCalcFactory) CreateOperator(op string, num1, num2 int) int {
+func CalcGetResult(h CalcER) int {
+	return h.GetResult()
+}
+
+type CalcFactory struct {
+}
+
+func (cc *CalcFactory) Create(op string, num1, num2 int) int {
 
 	switch op {
 	case "+":
-		var addCalc ADDCalc = ADDCalc{CaleCommon{
+		var addCalc AddCalc = AddCalc{BaseCalc{
 			num1: num1,
 			num2: num2,
 		}}
 		return CalcGetResult(&addCalc)
 	case "-":
-		var reduceCalc ReduceCalc = ReduceCalc{CaleCommon{
+		var reduceCalc ReduceCalc = ReduceCalc{BaseCalc{
 			num1: num1,
 			num2: num2,
 		}}
@@ -50,12 +61,4 @@ func (cc *CCalcFactory) CreateOperator(op string, num1, num2 int) int {
 		return 0
 	}
 
-}
-
-type CCalcEr interface {
-	GetResult() int
-}
-
-func CalcGetResult(h CCalcEr) int {
-	return h.GetResult()
 }
