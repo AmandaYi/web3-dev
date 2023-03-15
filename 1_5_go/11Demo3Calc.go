ccd78bb4019e52de6c22890a27beac3ebf9a3f44
AmandaYi <891177434@qq.com>
2023-03-15 17:35:38 +0800

    calc多态

--- 1_5_go/11Demo3Calc.go
+++ 1_5_go/11Demo3Calc.go
@@ -9,36 +9,43 @@ func main() {
 
 //多态主要解决的是需求改变后，需要修改对象创建代码的问题
 //使用工厂类，内部调用多态函数，即可解决此问题
-
+//定义基础类，比如打印机
 type BaseCalc struct {
 	num1 int
 	num2 int
 }
 
+//定义高级类，比如彩色打印机
 type AddCalc struct {
 	BaseCalc
 }
 
+//定义高级类，比如黑色打印机
 type ReduceCalc struct {
 	BaseCalc
 }
 
-func (ac *AddCalc) GetResult() int {
-	return ac.num1 + ac.num2
-}
-
-func (rc *ReduceCalc) GetResult() int {
-	return rc.num1 - rc.num2
-}
-
+//定义接口
 type CalcER interface {
 	GetResult() int
 }
 
+//定义接口多态方法
 func CalcGetResult(h CalcER) int {
 	return h.GetResult()
 }
 
+//实现接口方法,用来匹配接口
+func (ac *AddCalc) GetResult() int {
+	return ac.num1 + ac.num2
+}
+
+//实现接口方法,用来匹配接口
+func (rc *ReduceCalc) GetResult() int {
+	return rc.num1 - rc.num2
+}
+
+//创建工厂类,用于调用创建高级类,调用多态方法
 type CalcFactory struct {
 }
 
