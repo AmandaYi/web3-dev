4bf510498a9959c19903ff3aa443bd42448dc842
AmandaYi <891177434@qq.com>
2023-03-27 17:31:21 +0800

    chat room

--- 2_3_go/06chat_room.go
+++ 2_3_go/06chat_room.go
@@ -5,21 +5,30 @@ import (
 	"net"
 )
 
-//全部人员列表
-var allPerson map[string]person
-
 //每个人的基本信息
 type person struct {
-	name string
-	addr string
+	msgChan chan string
+	name    string
+	addr    string
 }
 
+//全部人员列表
+var allActivePerson map[string]person
+
+//全局消息
+var allMessage chan string
+
 func main() {
+
 	//创建tcp
 	listen, err := createTCPBaseSocket()
 	if err != nil {
 		return
 	}
+
+	// 创建管理者go程，管理map 和全局channel
+	go managerAllChannel()
+
 	for {
 		//监听连接，三次握手
 		conn, err := createConnAcceptSocket(listen)
@@ -29,8 +38,20 @@ func main() {
 		//处理用户逻辑
 		chatHandle(conn)
 	}
-}
 
+}
+func managerAllChannel() {
+	allActivePerson = make(map[string]person)
+
+	// 监听全局channel 中是否有数据, 有数据存储至 msg， 无数据阻塞。
+	for {
+		msg := <-allMessage
+		//把消息发给所有人的信道，让每个人自己的conn自行处理数据
+		for _, v := range allActivePerson {
+			v.msgChan <- msg
+		}
+	}
+}
 func createTCPBaseSocket() (net.Listener, error) {
 	listen, err := net.Listen("tcp", "127.0.0.1:9000")
 	if err != nil {
@@ -51,6 +72,25 @@ func createConnAcceptSocket(listener net.Listener) (net.Conn, error) {
 func chatHandle(conn net.Conn) {
 	//打印用户登录成功信息
 	fmt.Println("用户登录成功", conn.RemoteAddr().String())
-	//通知所有的人
+	//1 保存新用户信息到全体成员列表
+	addr := conn.RemoteAddr().String()
+	p := person{
+		msgChan: make(chan string),
+		name:    addr,
+		addr:    addr,
+	}
+	allActivePerson[addr] = p
+	//2 创建一个Go程，专门用于发消息
+	go func() {
+		fmt.Println("go func")
+		for {
+			msg := <-allActivePerson[addr].msgChan // 阻塞自己的消息通道，用于自行处理数据
+
+			conn.Write([]byte(msg + "\n"))
+		}
+	}()
+
+	//2 通知所有人该用户上线了，写入这个用户的的信道里面
+	allMessage <- p.name + " > 用户上线了"
 
 }
